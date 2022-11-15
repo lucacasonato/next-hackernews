1d58a1414a1094e6427b9ec7f00154d5cf58a286
Luca Casonato <hello@lcas.dev>
2022-11-15 22:12:31 +0100

    cache api

--- api.ts
+++ api.ts
@@ -2,9 +2,19 @@ const story = (path: string) => `https://node-hnapi.herokuapp.com/${path}`;
 const user = (path: string) =>
   `https://hacker-news.firebaseio.com/v0/${path}.json`;
 
+const cache = new Map();
+
 export default function fetchAPI(path: string) {
   const url = path.startsWith("user") ? user(path) : story(path);
   const headers = { "User-Agent": "chrome" };
 
-  return fetch(url, { headers }).then((r) => r.json());
+  const cached = cache.get(url);
+  if (cached) {
+    return cached;
+  }
+
+  const promise = fetch(url, { headers }).then((r) => r.json());
+  cache.set(url, promise);
+
+  return promise;
 }
